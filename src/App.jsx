d23ced4b4be85bc7089ef24e6674597b6883860f
Yosber Guerrero <yosbergc@gmail.com>
2024-05-23 13:20:52 -0400

    Initial Ecommerce Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='ext-3xl font-bold underline'>
+      <h1 className="text-3x1 text-blue-700">Testing</h1>
+    </div>
+  )
+}
+
+export default App
